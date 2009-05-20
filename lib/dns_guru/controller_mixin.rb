6ba6b695f6826370952b8344ccf2ee84cba4a345
Kenneth Miller <ken@siren.local>
2009-05-20 15:27:03 -0700

    accomodating url_for's asinine way of handling nonstandard ports

--- lib/dns_guru/controller_mixin.rb
+++ lib/dns_guru/controller_mixin.rb
@@ -2,7 +2,11 @@
 module DnsGuru
 	module ControllerMixin
 		def host_name(options = {})
-			request.canonical_host_name(options)
+			host = request.canonical_host_name(options)
+			if request.port && request.port != 80
+				host = "#{host}:#{request.port}"
+			end
+			host
 		end
 		def host_name_params
 			request.host_name_params
--- test/dns_guru/test_controller_mixin.rb
+++ test/dns_guru/test_controller_mixin.rb
@@ -17,6 +17,12 @@ module DnsGuru
 			assert_equal "www.mmp.com", @controller.host_name
 		end
 
+		def test_host_name_with_port
+			@request.host = "www.production.mmp.com"
+			@request.port = 3000
+			assert_equal "www.mmp.com:3000", @controller.host_name
+		end
+
 		def test_host_name_params
 			@request.host = "www.production.mamapedia.com"
 			assert_equal({:app => 'www', :stage => 'production', :brand => 'mamapedia', :tld => 'com'},
@@ -32,6 +38,7 @@ module DnsGuru
 	class MockRequest
 		include RequestMixin
 		attr_accessor :host
+		attr_accessor :port
 	end
 
 	class MockController

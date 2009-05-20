40a915c09eeb8674ed674260881697aa2bbb9b26
Kenneth Miller <ken@siren.local>
2009-05-20 15:00:52 -0700

    helper_method does not work at abstract level :(

--- init.rb
+++ init.rb
@@ -7,8 +7,6 @@ DnsGuru.init(config_file)
 
 class ActionController::AbstractRequest
 	include DnsGuru::RequestMixin
-	helper_method :host_name
-	helper_method :host_name_params
 end
 
 class ActionController::Base

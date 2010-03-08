96839acb5928628fa5075f478350c2cc13972f1e
Ken Miller <ken@corp.mamasource.com>
2010-03-07 16:36:57 -0800

    making the search for hosts.rb file more forgiving, and updating gem version

--- rails/init.rb
+++ rails/init.rb
@@ -4,8 +4,23 @@ require 'dns_guru'
 require 'rack'
 require 'action_controller/test_process'
 
-config_file = File.join(File.dirname(__FILE__), "../../../config/hosts.rb")
-DnsGuru.init(config_file)
+begin
+	config_files = [
+		File.join(File.dirname(__FILE__), "..","..","..","config","hosts.rb"),
+		File.join(File.dirname(__FILE__), "..","..","..","..","config","hosts.rb")
+	]
+	if Object.const_defined?(:Rails)
+		config_files << File.join(Rails.root, "config","hosts.rb")
+	end
+	config_files.detect do |config_file|
+		if File.exists?(config_file)
+			DnsGuru.init(config_file)
+			true
+		end
+	end
+rescue Exception => e
+	puts "Could not load dns_guru config file (hosts.rb): #{e}"
+end
 
 class Rack::Request
 	include DnsGuru::RequestMixin
